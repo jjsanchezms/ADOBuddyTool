3d4318b57720ef708d53083a0448491634df096d
Juan Jose Sanchez <sanchezjuan@microsoft.com>
2025-06-10 09:31:09 -0600

    chore: Update SDK and NuGet tool versions; remove obsolete library pack paths

--- obj/Debug/net8.0/CreateRoadmapADO.AssemblyInfo.cs
+++ obj/Debug/net8.0/CreateRoadmapADO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CreateRoadmapADO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e596926ba8401c0aaec48bcf8bde77e13fc8937")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c15dae190cae9fa365bac0bf515dc4362dea9622")]
 [assembly: System.Reflection.AssemblyProductAttribute("CreateRoadmapADO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CreateRoadmapADO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
