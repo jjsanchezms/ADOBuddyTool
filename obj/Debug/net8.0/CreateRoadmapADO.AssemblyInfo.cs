c5f038fc5b39affffc613ab05104e6c90d24ac67
Juan Jose Sanchez <sanchezjuan@microsoft.com>
2025-06-10 11:40:14 -0600

    chore: Update assembly information and regenerate binaries

--- obj/Debug/net8.0/CreateRoadmapADO.AssemblyInfo.cs
+++ obj/Debug/net8.0/CreateRoadmapADO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CreateRoadmapADO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7231b51c3fd58c82b0a47a9ffa3b8c1dbde350c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d2131fac920d00446dba9b3a05b34635bf2aced")]
 [assembly: System.Reflection.AssemblyProductAttribute("CreateRoadmapADO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CreateRoadmapADO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
